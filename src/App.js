715670c289e00d933a4b2f07c291e15e77a4d67a
Diego Urbina <70447898+yhonex@users.noreply.github.com>
2022-03-27 18:02:03 -0300

    cambios varios

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> prueba cambios.!
+          Edit <code>src/App.js</code> test 02 cambios.!
         </p>
         <a
           className="App-link"
